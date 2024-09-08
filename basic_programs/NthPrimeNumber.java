c4ed81abf7be0999a8d759184cb35b7b8c3788a7
Jasmine Mary <jasminemary630@gmail.com>
2024-09-08 23:34:58 +0530

    Palindrome

--- basic_programs/NthPrimeNumber.java
+++ basic_programs/NthPrimeNumber.java
@@ -1,32 +1,53 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class NthPrimeNumber {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 50;
 
         System.out.println(Arrays.toString(printPrimeNumbers(n)));
     }
 
     public static int[] printPrimeNumbers(int n){
-        if(n == 0){
+        if(n <= 1){
             return new int[]{};
         }
-        if(n == 1){
-            return new int[]{1};
+        if(n == 2){
+            return new int[]{2};
         }
 
-        int[] result = {2};
+        ArrayList<Integer> list = new ArrayList<>();
 
-        for(int i = 2; i <= 10; i++){
+        for(int i = 2; i <= n; i++){
 
-             while(i <= Math.sqrt(i)){
-                if(i % 2 == 0){
-                    result.push()
-                }
+             if(isPrimeNumber(i)){
+                list.add(i);
              }
+        }
+
+        int[] result = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            result[i] = list.get(i);
         }
 
 
         return result;
+    }
+
+    public static boolean isPrimeNumber(int number){
+        if(number < 2){
+            return false;
+        }
+
+        int i = 2;
+
+        while(i <= Math.sqrt(number)){
+            if(number % i == 0){
+                return false;
+            }
+            i++;
+        }
+
+        return true;
     }
 }
