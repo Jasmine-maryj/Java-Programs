a0b66ad2d9288e123f9c0187d031ab21461d02d7
Jasmine Mary <jasminemary630@gmail.com>
2024-09-18 22:20:55 +0530

    bubble sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,5 +1,29 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
-    
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 5, 4, 2};
+
+        System.out.println(Arrays.toString(bubbleSort(arr)));
+    }
+
+    public static int[] bubbleSort(int[] arr){
+        if(arr.length == 1){
+            return arr;
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 1; j < arr.length - i; j++){
+                if(arr[j] < arr[j - 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
 }
